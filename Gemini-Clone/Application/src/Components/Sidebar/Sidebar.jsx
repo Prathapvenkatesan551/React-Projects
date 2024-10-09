5c2a666262510bb79ba4d151dc47dc582802fa6a
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-09 22:23:12 +0530

    css

--- Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
+++ Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
@@ -133,6 +133,7 @@ const Sidebar = () => {
                 <p>Coimbatore , Tamilnadu , India</p>
                 <p>From your Ip address : updated location</p>
             </div>
+            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
         </div>
       
     </div>
