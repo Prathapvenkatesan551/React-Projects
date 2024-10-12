1e09aef4d517a8174abee20e1bdeadec4bc8ef33
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-12 22:07:31 +0530

    card.jsx

---
+++ Gemini-Clone/Application/src/Components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Cards.css'
+const Cards = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cards
--- Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
+++ Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
@@ -134,11 +134,7 @@ const Sidebar = () => {
                 <p>From your Ip address : updated location</p>
             </div>
             <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
-            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
-            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
-            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
-            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
-            <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
+
         </div>
       
     </div>
