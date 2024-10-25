d8ab5c4fc5b179b4a357d42199573a6f5cddb9cf
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-25 23:29:33 +0530

    c-1

--- Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
+++ Gemini-Clone/Application/src/Components/Sidebar/Sidebar.jsx
@@ -111,6 +111,30 @@ const Sidebar = () => {
                 <img src="message_icon.png" alt="" />
                 <p>What is Java ...</p>
             </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
+            <div className='recent-search'>
+                <img src="message_icon.png" alt="" />
+                <p>What is Java ...</p>
+            </div>
             </div>
            
         </div>
--- Gemini-Clone/Application/src/main.jsx
+++ Gemini-Clone/Application/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextProvider from './Context/Contex.jsx'
-  // const {onSent,recentPrompt}
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
     <App />
